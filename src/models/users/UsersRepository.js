8284f2056d0886383404116d8afd3cda8f077805
pablodelgado26 <158165561+pablodelgado26@users.noreply.github.com>
2024-10-31 10:47:29 -0300

    feat: criação de metodo de busca por id com get

--- src/models/users/UsersRepository.js
+++ src/models/users/UsersRepository.js
@@ -8,6 +8,18 @@ class UsersRepository {
     getALLUsers() {
         return this.users;
     }
+
+    addUser(name, email, passoword) {
+        const newUser = new User(name, email, passoword);
+        this.users.push(newUser);
+        return newUser;
+    }
+
+    getUserById(id) {
+        const user = this.users.find((U) => U.id == id);
+
+        return user;
+    }
 }
 
 export default UsersRepository;
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -17,4 +17,39 @@ usuariosRoutes.get("/", (req, res) => {
     });
 });
 
+usuariosRoutes.post("/", (req, res) => {
+    const { name, email, password } = req.body;
+
+    const usuario = usersRepository.addUser(name, email, password);
+
+    return res.status(201).json({
+        messege: "Usuário cadastrado com sucesso!",
+        usuario,
+    });
+});
+
+usuariosRoutes.get("/:id", (req, res) => {
+    const { id } = req.params;
+    const user = usersRepository.getUserById(id);
+
+    if (!user) {
+        return res.status(404).json({
+            messege: `Usuário com id ${id} não encontrado!`,
+        });
+    }
+
+    return res.status(200).json({
+        messege: `Usuário com id ${id} encontrado!`,
+        user,
+    });
+});
+
+
+usuariosRoutes.put("/:id", (req, res) => {
+});
+
+
+usuariosRoutes.delete("/:id", (req, res) => {
+});    
+
 export default usuariosRoutes;
