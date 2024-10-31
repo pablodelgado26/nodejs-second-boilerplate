459154e07c24d058c39bd23c668482fcbcdc387a
pablodelgado26 <158165561+pablodelgado26@users.noreply.github.com>
2024-10-31 08:20:27 -0300

    feat: colocando usuarios

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -2,16 +2,26 @@ import { Router } from "express";
 
 const usuariosRoutes = Router();
 
-let usuarios = [];
+let usuarios = [
+    { 
+        id: 1, 
+        nome: "João", 
+        email: "joao@example.com",
+    },
+    {
+        id: 2,
+        nome: "Maria",
+        email: "maria@example.com"
+    }
+];
 
-    usuariosRoutes.get("/", (req, res) => {
-        return res.status(200).json({
-            messege: usuarios.length == 0 
-            ? "Nenhum usuário cadastrado" 
-            : `total de usuários cadastrados: $ 
-            {usuarios.length}`,	 
-            usuarios,            
-        });
+usuariosRoutes.get("/", (req, res) => {
+    return res.status(200).json({
+        messege: usuarios.length == 0 
+        ? "Nenhum usuário cadastrado" 
+        : `total de usuários cadastrados: ${usuarios.length}`,	 
+        usuarios,            
     });
+});
 
 export default usuariosRoutes;
